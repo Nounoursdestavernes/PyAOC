544c044af66cf315168c0b1ff6e72462417b5669
Aurélien <troncy.aurelien@gmail.com>
2023-12-30 17:47:52 +0100

    [fix]: regexp to collect title doesn't work when day_number > 9

--- src/pyaoc/day_subject.py
+++ src/pyaoc/day_subject.py
@@ -14,7 +14,7 @@ def get_day_name(day_number: int = 1, year_number: int = 2023) -> str:
     url = f"https://adventofcode.com/{year_number}/day/{day_number}"
 
     req = requests.get(url)
-    full_title = re.findall("<h2>---.*Day \d:.*---</h2>", req.text)[0] # To get the full day title
+    full_title = re.findall("<h2>---.*Day \d+:.*---</h2>", req.text)[0] # To get the full day title
     title = re.findall(":\s.*\s", full_title)[0][2:-1]
 
     return title

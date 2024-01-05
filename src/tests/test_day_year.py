363e387a9d65e50d0dcd92748c29f026df29a2f9
Aurélien <troncy.aurelien@gmail.com>
2024-01-05 18:37:53 +0100

    [feat]: tests for day_year.py

--- src/tests/test_day_year.py
+++ src/tests/test_day_year.py
@@ -4,4 +4,4 @@ from pyaoc.day_year import get_last_aoc_year
 def test_get_last_aoc_year():
     # This test must be valid until 1st decembre 2024 (at 0am EST)
     assert get_last_aoc_year() == 2023
-    
+

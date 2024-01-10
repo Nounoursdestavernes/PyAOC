94f437dbc27a7b85be7399d44552c3fa4ccc8cc2
Aurélien <troncy.aurelien@gmail.com>
2024-01-10 15:51:15 +0100

    [feat] use sys.exit instead of return and let the main code handle everything

--- src/pyaoc/day_creation.py
+++ src/pyaoc/day_creation.py
@@ -1,63 +1,51 @@
 # This file contains functions to create a new day for Advent of Code
-import logging
 import os
 import shutil
+import sys
 from jinja2 import Environment, PackageLoader
+from pyaoc.check_parameters import check_day_number
 
 #Jinja2 environment
 env = Environment(
     loader=PackageLoader('pyaoc', '.'),
 )
 
-# Logger
-logging.basicConfig(level=logging.WARNING, format='%(levelname)s %(message)s')
-
-def create_day(day_number: int = 1) -> int:
+def create_day(day_number: int = 1) -> None:
     """Create a new directory for a new day of Advent of Code.
 
-    Returns an int that represents if an error occured:
-        * 0: No error
-        * 1: Error
+    Returns None if the creation is successful, else sys.exit.
     
     :param int day_number: Number of the day. Must be between 1 and 25.
 
-    :return: error
-    :rtype: int
+    :return: None
+    :rtype: None
     """
-    if type(day_number) != int:
-        logging.error("Invalid day number : day_number must be an integer")
-        return 1
 
     # Check if the day number is valid
-    if day_number < 1 or day_number > 25:
-        logging.error("Invalid day number : day_number must be between 1 and 25")
-        return 1
-    
+    check_day_number(day_number)
+
     # Create the directory
     dir_name = f"day{day_number:02d}"
 
 
     # Create folders
     if os.path.exists(dir_name):
-        logging.error(f"Directory {dir_name} already exists")
-        return 1
+        return sys.exit(f"Directory {dir_name} already exists")
     
     os.mkdir(dir_name)
     os.chdir(dir_name)
 
     # Input folder
     if os.path.exists("inputs"):
-        logging.error("inputs folder already exists")
         os.chdir("..")
-        return 1
+        return sys.exit("inputs folder already exists")
     
     os.mkdir("inputs")
 
     # Benchmark folder
     if os.path.exists("benchmark"):
-        logging.error("benchmark folder already exists")
         os.chdir("..")
-        return 1
+        return sys.exit("benchmark folder already exists")
     
     os.mkdir("benchmark")
 
@@ -85,30 +73,23 @@ def create_day(day_number: int = 1) -> int:
     # return to the root directory
     os.chdir("..")
 
-    return 0
+    return None # No error
 
 
 
 def create_day_force(day_number: int = 1) -> int:
     """Create a new directory for a new day of Advent of Code. If the directory already exists, delete it and create a new one.
     
-    Returns an int that represents if an error occured:
-        * 0: No error
-        * 1: Error
-    
+    Returns None if the creation is successful, else sys.exit.
+
     :param int day_number: Number of the day. Must be between 1 and 25.
 
-    :return: error
-    :rtype: int
+    :return: None
+    :rtype: None
     """
-    if type(day_number) != int:
-        logging.error("Invalid day number : day_number must be an integer")
-        return 1
-
+   
     # Check if the day number is valid
-    if day_number < 1 or day_number > 25:
-        logging.error("Invalid day number : day_number must be between 1 and 25")
-        return 1
+    check_day_number(day_number)
     
     # Create the directory
     dir_name = f"day{day_number:02d}"
@@ -157,4 +138,4 @@ def create_day_force(day_number: int = 1) -> int:
     # return to the root directory
     os.chdir("..")
 
-    return 0 # No error
+    return None # No error
--- src/tests/test_day_creation.py
+++ src/tests/test_day_creation.py
@@ -1,49 +1,68 @@
-from pyaoc.day_creation import create_day, create_day_force
+import pytest
 import shutil
+from pyaoc.day_creation import create_day, create_day_force
 
 # We assume that no day folder exists (clean environment)
 
 # Tests for create_day
 def test_create_day():
-    assert create_day(1) == 0
+    assert create_day(1) == None
     shutil.rmtree("day01")
 
 def test_create_day_already_exists():
-    assert create_day(1) == 0
-    assert create_day(1) == 1
+    assert create_day(1) == None
+    with pytest.raises(SystemExit) as pytest_wrapped_e: # We expect a SystemExit
+        create_day(1)
+    assert pytest_wrapped_e.type == SystemExit
     shutil.rmtree("day01")
 
 def test_create_day_out_of_range():
-    assert create_day(0) == 1
-    assert create_day(26) == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        create_day(0)
+    assert pytest_wrapped_e.type == SystemExit
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        create_day(26)
+    assert pytest_wrapped_e.type == SystemExit
 
 def test_create_day_negative():
-    assert create_day(-1) == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        create_day(-1)
+    assert pytest_wrapped_e.type == SystemExit
 
 def test_create_day_no_args():
-    assert create_day() == 0
+    assert create_day() == None
     shutil.rmtree("day01")
 
 def test_create_day_not_int():
-    assert create_day("a") == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        create_day("a")
+    assert pytest_wrapped_e.type == SystemExit
 
 
 # Tests for create_day_force
 def test_create_day_force():
-    assert create_day_force(1) == 0
-    assert create_day_force(1) == 0
+    assert create_day_force(1) == None
+    assert create_day_force(1) == None
     shutil.rmtree("day01")
 
 def test_create_day_force_out_of_range():
-    assert create_day_force(0) == 1
-    assert create_day_force(26) == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        create_day_force(0)
+    assert pytest_wrapped_e.type == SystemExit
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        create_day_force(26)
+    assert pytest_wrapped_e.type == SystemExit
 
 def test_create_day_force_negative():
-    assert create_day_force(-1) == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        create_day_force(-1)
+    assert pytest_wrapped_e.type == SystemExit
 
 def test_create_day_force_no_args():
-    assert create_day_force() == 0
+    assert create_day_force() == None
     shutil.rmtree("day01")
 
 def test_create_day_force_not_int():
-    assert create_day_force("a") == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        create_day_force("a")
+    assert pytest_wrapped_e.type == SystemExit

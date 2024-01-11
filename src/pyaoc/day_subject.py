94337f40f74517d4bddb12d3e55e6f51f1eef3e4
Aurélien <troncy.aurelien@gmail.com>
2024-01-11 15:20:25 +0100

    [feat/fix]: new format with sys.exit and add submit function

--- src/pyaoc/day_subject.py
+++ src/pyaoc/day_subject.py
@@ -4,6 +4,7 @@ import os
 import requests
 import re
 import sys
+from pyaoc.check_parameters import check_day_number, check_year_number, check_part_number
 
 
 logging.basicConfig(level=logging.WARNING, format='%(levelname)s %(message)s')
@@ -15,38 +16,30 @@ def get_day_name(day_number: int = 1, year_number: int = 2023) -> str:
     :param int day_number: Number of the day. Must be between 1 and 25
     :param int year_number: Number of the year. Must be between 2015 and current year
 
-    :return: day name
+    :return: day_name
     :rtype: str
     """
 
-    if type(day_number) != int:
-        logging.error("Invalid day number : day_number must be an int")
-        sys.exit(1)
+    check_day_number(day_number)
     
-    if type(year_number) != int:
-        logging.error("Invalid year number : year_number must be an int")
-        sys.exit(1)
-    
+    check_year_number(year_number)
 
     url = f"https://adventofcode.com/{year_number}/day/{day_number}"
 
     req = requests.get(url)
 
     if req.status_code != 200:
-        logging.error(f"Error {req.status_code} while getting day name")
-        sys.exit(1)
+        sys.exit(f"Error {req.status_code} while getting day name")
 
-    full_title = re.findall("<h2>---.*Day [0-9]+:.*---</h2>", req.text)[0] # To get the full day title
-    title = re.findall(":[ \t\n\r\f\v].*[ \t\n\r\f\v]", full_title)[0][2:-1]
+    h2_title = re.findall("<h2>---.*Day [0-9]+:.*---</h2>", req.text)[0] # To get the full day title
+    day_name = re.findall(":[ \t\n\r\f\v].*[ \t\n\r\f\v]", h2_title)[0][2:-1]
 
-    return title
+    return day_name
 
-def get_input(day_number: int = 1, year_number: int = 2023) -> int:
+def get_input(day_number: int = 1, year_number: int = 2023) -> None:
     """Get the input of a specific day of a specific year of the Advent Of Code
     
-    Returns an int that represents if an error occured:
-        * 0: No error
-        * 1: Error
+    Returns None if the input is successfully downloaded, else sys.exit
 
     :param int day_number: Number of the day. Must be between 1 and 25
     :param int year_number: Number of the year. Must be between 2015 and current year
@@ -55,47 +48,37 @@ def get_input(day_number: int = 1, year_number: int = 2023) -> int:
     :rtype: int
     """
 
-    if type(day_number) != int:
-        logging.error("Invalid day number : day_number must be an int")
-        return 1
-    
-    if type(year_number) != int:
-        logging.error("Invalid year number : year_number must be an int")
-        return 1
+    check_day_number(day_number)
+
+    check_year_number(year_number)
 
     if not os.path.exists("secret.txt"):
-        logging.error("secret.txt does not exist")
-        return 1
+        sys.exit("secret.txt does not exist")
     
     cookie = open("secret.txt", 'r').read().strip()
 
-    
     dir_name = f"day{day_number:02d}"
     if not os.path.exists(dir_name):
-        logging.error(f"Directory {dir_name} does not exist")
-        return 1
+        sys.exit(f"Directory {dir_name} does not exist")
     
     os.chdir(dir_name)
 
     if not os.path.exists("inputs"):
-        logging.error(f"Directory {dir_name}/inputs does not exist")
         os.chdir("..")
-        return 1
+        sys.exit(f"Directory {dir_name}/inputs does not exist")
     
     os.chdir("inputs")
     
     if not os.path.exists("input.txt"):
-        logging.error(f"File {dir_name}/inputs/input.txt does not exist")
         os.chdir("..") # Go back to day folder
         os.chdir("..") # Go back to src folder
-        return 1
-    
+        sys.exit(f"File {dir_name}/inputs/input.txt does not exist")
+
     url = f"https://adventofcode.com/{year_number}/day/{day_number}/input"
     req = requests.get(url, cookies={"session": cookie})
 
     if req.status_code != 200:
-        logging.error(f"Error {req.status_code} while getting input")
-        return 1
+        sys.exit(f"Error {req.status_code} while getting input")
     
     with open("input.txt", "w") as f:
         f.write(req.text)
@@ -103,5 +86,53 @@ def get_input(day_number: int = 1, year_number: int = 2023) -> int:
     os.chdir("..") # Go back to day folder
     os.chdir("..") # Go back to src folder
     
-    return 0
+    return None
 
+def submit(part_number: int = 1, day_number: int = 1, year_number: int = 2023, res: int = 0) -> None:
+    """Submit the result of a specific part of a specific day of a specific year of the Advent Of Code
+
+    Returns None if the submission is successful, else sys.exit
+
+    :param int part_number: Number of the part. Must be between 1 and 2
+    :param int day_number: Number of the day. Must be between 1 and 25
+    :param int year_number: Number of the year. Must be between 2015 and current year
+    :param int res: Result to submit
+
+    :return: error
+    :rtype: int
+    """
+
+    check_day_number(day_number)
+
+    check_part_number(part_number)
+
+    check_year_number(year_number)
+
+    if type(res) != int:
+        sys.exit("Invalid result : res must be an int")
+
+    if not os.path.exists("secret.txt"):
+        sys.exit("secret.txt does not exist")
+
+    cookie = open("secret.txt", 'r').read().strip()
+    
+    url = f"https://adventofcode.com/{year_number}/day/{day_number}/answer"
+    req = requests.post(url, cookies={"session": cookie}, data={"answer": res, 'level': part_number})
+
+    if req.status_code != 200:
+        sys.exit(f"Error {req.status_code} while submitting answer")
+    
+    if "That's the right answer" in req.text:
+        print("Correct answer !")
+    elif "That's not the right answer" in req.text:
+        print("Wrong answer !")
+    elif "You gave an answer too recently" in req.text:
+        print("You gave an answer too recently !")
+        time = re.findall("You have .* left to wait", req.text)[0]
+        print(time)
+    elif "You don't seem to be solving the right level" in req.text:
+        print("You already solved this level !")
+    else:
+        print("Unknown error !")
+
+    return None
--- src/tests/test_day_subject.py
+++ src/tests/test_day_subject.py
@@ -1,7 +1,7 @@
 import pytest
 import os
 import shutil
-from pyaoc.day_subject import get_day_name, get_input
+from pyaoc.day_subject import get_day_name, get_input, submit
 
 # Tests for get_day_name
 
@@ -60,21 +60,29 @@ def test_get_day_name_not_int():
     
 def test_get_input_no_secret():
     os.rename("secret.txt", "secret.txt.pyaoc")
-    assert get_input() == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input()
+    assert pytest_wrapped_e.type == SystemExit
     os.rename("secret.txt.pyaoc", "secret.txt")
 
 def test_get_input_no_day_folder():
-    assert get_input() == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input()
+    assert pytest_wrapped_e.type == SystemExit
 
 def test_get_input_no_input_folder():
     os.mkdir("day01")
-    assert get_input() == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input()
+    assert pytest_wrapped_e.type == SystemExit
     os.rmdir("day01")
 
 def test_get_input_no_input_file():
     os.mkdir("day01")
     os.mkdir(os.path.join("day01", "inputs"))
-    assert get_input() == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input()
+    assert pytest_wrapped_e.type == SystemExit
     shutil.rmtree("day01")
 
 def test_get_input():
@@ -82,38 +90,103 @@ def test_get_input():
     os.mkdir(os.path.join("day01", "inputs"))
     with open(os.path.join("day01", "inputs", "input.txt"), "w") as f:
         pass
-    assert get_input() == 0
+    assert get_input() == None
     shutil.rmtree("day01")
 
 def test_get_input_negative_day():
-    assert get_input(day_number=-1) == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input(day_number=-1)
+    assert pytest_wrapped_e.type == SystemExit
 
 def test_get_input_negative_year():
-    assert get_input(year_number=-1) == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input(year_number=-1)
+    assert pytest_wrapped_e.type == SystemExit
 
 def test_get_input_out_of_range_day():
-    assert get_input(day_number=0) == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input(day_number=0)
+    assert pytest_wrapped_e.type == SystemExit
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input(day_number=26)
+    assert pytest_wrapped_e.type == SystemExit
 
 def test_get_input_out_of_range_year():
-    assert get_input(year_number=2014) == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input(year_number=2014)
+    assert pytest_wrapped_e.type == SystemExit
 
 def test_get_input_not_int_day():
-    assert get_input(day_number="a") == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input(day_number="a")
+    assert pytest_wrapped_e.type == SystemExit
 
 def test_get_input_not_int_year():
-    assert get_input(year_number="a") == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input(year_number="a")
+    assert pytest_wrapped_e.type == SystemExit
 
 def test_get_input_not_int_day_and_not_int_year():
-    assert get_input(day_number="a", year_number="a") == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input(day_number="a", year_number="a")
+    assert pytest_wrapped_e.type == SystemExit
 
 def test_get_input_not_int_day_and_int_year():
-    assert get_input(day_number="a", year_number=2020) == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input(day_number="a", year_number=2020)
+    assert pytest_wrapped_e.type == SystemExit
 
 def test_get_input_int_day_and_not_int_year():
-    assert get_input(day_number=1, year_number="a") == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input(day_number=1, year_number="a")
+    assert pytest_wrapped_e.type == SystemExit
 
 def test_get_input_past_year():
-    assert get_input(year_number=2014) == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input(year_number=2014)
+    assert pytest_wrapped_e.type == SystemExit
 
 def test_get_input_future_year():
-    assert get_input(year_number=50000) == 1
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        get_input(year_number=50000)
+    assert pytest_wrapped_e.type == SystemExit
+
+# Tests for submit
+
+def test_submit_no_secret():
+    os.rename("secret.txt", "secret.txt.pyaoc")
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        submit()
+    assert pytest_wrapped_e.type == SystemExit
+    os.rename("secret.txt.pyaoc", "secret.txt")
+
+def test_submit_out_of_range():
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        submit(part_number=0)
+    assert pytest_wrapped_e.type == SystemExit
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        submit(part_number=3)
+    assert pytest_wrapped_e.type == SystemExit
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        submit(day_number=0)
+    assert pytest_wrapped_e.type == SystemExit
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        submit(day_number=26)
+    assert pytest_wrapped_e.type == SystemExit
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        submit(year_number=0)
+    assert pytest_wrapped_e.type == SystemExit
+
+def test_submit_not_int():
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        submit(part_number="a")
+    assert pytest_wrapped_e.type == SystemExit
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        submit(day_number="a")
+    assert pytest_wrapped_e.type == SystemExit
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        submit(year_number="a")
+    assert pytest_wrapped_e.type == SystemExit
+
+def test_submit():
+    assert submit(year_number=2015) == None
